cd226d06bb87e7fa7fd94ab992527808c133a6b8
ryanharrishtx <139286619+ryanharrishtx@users.noreply.github.com>
2023-09-06 16:45:23 -0500

    complete, but still testing responsiveness

---
+++ script.js
@@ -0,0 +1,60 @@
+// Not sure if I want to use this, yet.
+
+// window.addEventListener('load', function() {
+//     setTimeout(function() {
+//         var fontFamilyChange = document.getElementById('text');
+//         fontFamilyChange.innerHTML = `HOLA!`;
+//     }, 1500);
+//     setTimeout(function() {
+//         var fontFamilyChange = document.getElementById('text');
+//         fontFamilyChange.innerHTML = `BONJOUR!`;
+//     }, 2000);
+//     setTimeout(function() {
+//         var fontFamilyChange = document.getElementById('text');
+//         fontFamilyChange.innerHTML = `CIAO!`;
+//     }, 2500);
+//     setTimeout(function() {
+//         var fontFamilyChange = document.getElementById('text');
+//         fontFamilyChange.innerHTML = `ALOHA!`;
+//     }, 3000);
+//     setTimeout(function() {
+//         var fontFamilyChange = document.getElementById('text');
+//         fontFamilyChange.innerHTML = `HELLO!`;
+//     }, 3500);
+//     });
+
+// function xSmall(x) {
+//     if (x.matches) {
+//         setTimeout(function() {
+//             var fontFamilyChange = document.getElementById('text');
+//             fontFamilyChange.innerHTML = `HOLA!`;
+//             fontFamilyChange.style.fontSize = '87px';
+
+//         }, 1500);
+//         setTimeout(function() {
+//             var fontFamilyChange = document.getElementById('text');
+//             fontFamilyChange.innerHTML = `BONJOUR!`;
+//             fontFamilyChange.style.fontSize = '87px';
+//         }, 1750);
+//         setTimeout(function() {
+//             var fontFamilyChange = document.getElementById('text');
+//             fontFamilyChange.innerHTML = `CIAO!`;
+//             fontFamilyChange.style.fontSize = '87px';
+//         }, 2000);
+//         setTimeout(function() {
+//             var fontFamilyChange = document.getElementById('text');
+//             fontFamilyChange.innerHTML = `ALOHA!`;
+//             fontFamilyChange.style.fontSize = '87px';
+//         }, 2250);
+//         setTimeout(function() {
+//             var fontFamilyChange = document.getElementById('text');
+//             fontFamilyChange.innerHTML = `HELLO!`;
+//             fontFamilyChange.style.fontSize = '87px';
+    
+//         }, 2500);
+//     }
+// }
+// var x= window.matchMedia("(max-width: 320px)");
+// xSmall(x);
+// x.addEventListener(xSmall);
+
